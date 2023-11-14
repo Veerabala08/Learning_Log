8c4482f2857212a8ad07481ac56f0391dad74299
veerabala <imveerabala@gmail.com>
2023-11-14 17:36:01 +0530

    new user can register

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #installed app
+    'learning_logs', 
+    'users', 
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_logs', #installed app
+    
 ]
 
 MIDDLEWARE = [
